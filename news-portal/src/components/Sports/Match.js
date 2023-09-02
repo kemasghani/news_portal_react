0d0f0d952783da83b6333f149cfe6c64e070b964
kemasghani <kemasghani123@gmail.com>
2023-09-02 23:42:25 +0700

    done feature weather

---
+++ news-portal/src/components/Sports/Match.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/**
+ * Live football match
+ * @param {*} props 
+ */
+const match = props => {
+    return (
+        <div className="margin-top-20" key={props.id}>
+            { props.status === "IN_PLAY" ?
+                <div className="inline red">{props.time} </div> : <div className="inline red">HT </div>
+            }
+            <div className="inline">{props.homeTeamName}   </div>  
+            <div className="inline red">{props.homeTeamScore} - {props.awayTeamScore}</div>  
+            <div className="inline">   {props.awayTeamName}</div>
+        </div>
+    )
+};
+
+export default match;
